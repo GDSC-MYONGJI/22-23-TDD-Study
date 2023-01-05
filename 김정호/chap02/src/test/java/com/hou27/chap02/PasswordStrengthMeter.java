3b47e296c378e9432c6d51bbe70c002646dde2fa
hou27 <ataj125@gmail.com>
2023-01-06 04:13:20 +0900

    첫번째 테스트 통과
    PasswordStrengthMeter 작성
    PasswordStrength 작성

---
+++ 김정호/chap02/src/test/java/com/hou27/chap02/PasswordStrengthMeter.java
@@ -0,0 +1,9 @@
+package com.hou27.chap02;
+
+public class PasswordStrengthMeter {
+
+    public PasswordStrength meter(String s) {
+      return PasswordStrength.STRONG;
+    }
+
+}
--- 김정호/chap02/src/test/java/com/hou27/chap02/PasswordStrengthMeterTest.java
+++ 김정호/chap02/src/test/java/com/hou27/chap02/PasswordStrengthMeterTest.java
@@ -1,5 +1,15 @@
 package com.hou27.chap02;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PasswordStrengthMeterTest {
+
+  @Test
+  void meetsAllCriteria_Then_Strong() {
+    PasswordStrengthMeter meter = new PasswordStrengthMeter();
+    PasswordStrength result = meter.meter("123ABCDE456");
+    assertEquals(PasswordStrength.STRONG, result);
+  }
 
 }
