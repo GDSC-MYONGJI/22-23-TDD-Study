92efd719226f54d8ada28192b6161abf1c6223a9
hou27 <ataj125@gmail.com>
2023-01-06 04:49:26 +0900

    두번째 테스트 통과

--- 김정호/chap02/src/test/java/com/hou27/chap02/PasswordStrengthMeter.java
+++ 김정호/chap02/src/test/java/com/hou27/chap02/PasswordStrengthMeter.java
@@ -3,7 +3,10 @@ package com.hou27.chap02;
 public class PasswordStrengthMeter {
 
     public PasswordStrength meter(String s) {
-      return PasswordStrength.STRONG;
+        if (s.length() < 8) {
+            return PasswordStrength.NORMAL;
+        }
+        return PasswordStrength.STRONG;
     }
 
 }
