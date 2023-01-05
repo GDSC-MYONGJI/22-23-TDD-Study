ac0ff74905e47b79f2427c91bb03da2aca9da3b8
hou27 <ataj125@gmail.com>
2023-01-06 05:00:06 +0900

    테스트 코드 리팩토링 - #3

--- 김정호/chap02/src/test/java/com/hou27/chap02/PasswordStrengthMeterTest.java
+++ 김정호/chap02/src/test/java/com/hou27/chap02/PasswordStrengthMeterTest.java
@@ -4,26 +4,35 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PasswordStrengthMeterTest {
+  private PasswordStrengthMeter meter = new PasswordStrengthMeter();
+
+  private void assertStrength(String password, PasswordStrength expStr) {
+    PasswordStrength result = meter.meter(password);
+    assertEquals(expStr, result);
+  }
 
   @Test
   void meetsAllCriteria_Then_Strong() {
-    PasswordStrengthMeter meter = new PasswordStrengthMeter();
-    PasswordStrength result = meter.meter("123ABCDE456");
-    assertEquals(PasswordStrength.STRONG, result);
+    assertStrength("123ABCDE456", PasswordStrength.STRONG);
+//    PasswordStrengthMeter meter = new PasswordStrengthMeter();
+//    PasswordStrength result = meter.meter("123ABCDE456");
+//    assertEquals(PasswordStrength.STRONG, result);
   }
 
   @Test
   void meetsOtherCriteria_except_for_Length_Then_Normal() {
-    PasswordStrengthMeter meter = new PasswordStrengthMeter();
-    PasswordStrength result = meter.meter("12345A");
-    assertEquals(PasswordStrength.NORMAL, result);
+    assertStrength("12345A", PasswordStrength.NORMAL);
+//    PasswordStrengthMeter meter = new PasswordStrengthMeter();
+//    PasswordStrength result = meter.meter("12345A");
+//    assertEquals(PasswordStrength.NORMAL, result);
   }
 
   @Test
   void meetsOtherCriteria_except_for_Number_Then_Normal() {
-    PasswordStrengthMeter meter = new PasswordStrengthMeter();
-    PasswordStrength result = meter.meter("ABCDEexceptNumber");
-    assertEquals(PasswordStrength.NORMAL, result);
+    assertStrength("ABCDEexceptNumber", PasswordStrength.NORMAL);
+//    PasswordStrengthMeter meter = new PasswordStrengthMeter();
+//    PasswordStrength result = meter.meter("ABCDEexceptNumber");
+//    assertEquals(PasswordStrength.NORMAL, result);
   }
 
 }
