2c8664bb1649815925aee43c95cac273ea1639a6
hou27 <ataj125@gmail.com>
2023-01-06 05:03:21 +0900

    암호 검사기 네번째 테스트 틀 작성

--- 김정호/chap02/src/test/java/com/hou27/chap02/PasswordStrengthMeterTest.java
+++ 김정호/chap02/src/test/java/com/hou27/chap02/PasswordStrengthMeterTest.java
@@ -14,25 +14,21 @@ public class PasswordStrengthMeterTest {
   @Test
   void meetsAllCriteria_Then_Strong() {
     assertStrength("123ABCDE456", PasswordStrength.STRONG);
-//    PasswordStrengthMeter meter = new PasswordStrengthMeter();
-//    PasswordStrength result = meter.meter("123ABCDE456");
-//    assertEquals(PasswordStrength.STRONG, result);
   }
 
   @Test
   void meetsOtherCriteria_except_for_Length_Then_Normal() {
     assertStrength("12345A", PasswordStrength.NORMAL);
-//    PasswordStrengthMeter meter = new PasswordStrengthMeter();
-//    PasswordStrength result = meter.meter("12345A");
-//    assertEquals(PasswordStrength.NORMAL, result);
   }
 
   @Test
   void meetsOtherCriteria_except_for_Number_Then_Normal() {
     assertStrength("ABCDEexceptNumber", PasswordStrength.NORMAL);
-//    PasswordStrengthMeter meter = new PasswordStrengthMeter();
-//    PasswordStrength result = meter.meter("ABCDEexceptNumber");
-//    assertEquals(PasswordStrength.NORMAL, result);
+  }
+
+  @Test
+  void nullInput_Then_Invalid() {
+    assertStrength(null, PasswordStrength.INVALID);
   }
 
 }
