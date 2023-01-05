3d1b683179380230436375a4af304decd49542b9
kyubeom <be_man@naver.com>
2023-01-06 02:34:00 +0900

    불 필요한 import 제거 - #6

--- 한규범/chap02/src/test/java/chap02/PasswordStrengthMeterTest.java
+++ 한규범/chap02/src/test/java/chap02/PasswordStrengthMeterTest.java
@@ -1,9 +1,8 @@
 package chap02;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PasswordStrengthMeterTest {
 
