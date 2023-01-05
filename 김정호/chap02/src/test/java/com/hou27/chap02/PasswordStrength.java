1e79eda46772ab981aa09334e6724a4c04aa1c2e
hou27 <ataj125@gmail.com>
2023-01-06 05:39:12 +0900

    여섯 번째 테스트 통과

--- 김정호/chap02/src/test/java/com/hou27/chap02/PasswordStrength.java
+++ 김정호/chap02/src/test/java/com/hou27/chap02/PasswordStrength.java
@@ -1,5 +1,5 @@
 package com.hou27.chap02;
 
 public enum PasswordStrength {
-  STRONG, NORMAL, INVALID
+  STRONG, NORMAL, INVALID, WEAK
 }
--- 김정호/chap02/src/test/java/com/hou27/chap02/PasswordStrengthMeter.java
+++ 김정호/chap02/src/test/java/com/hou27/chap02/PasswordStrengthMeter.java
@@ -2,14 +2,28 @@ package com.hou27.chap02;
 
 public class PasswordStrengthMeter {
 
-    public PasswordStrength meter(String s) {
-        if(s == null || s.isBlank()) {
-            return PasswordStrength.INVALID;
-        }
-        else if (s.length() < 8 || !s.matches(".*[0-9].*") || !s.matches(".*[A-Z].*")) {
-            return PasswordStrength.NORMAL;
-        }
-        return PasswordStrength.STRONG;
+  public PasswordStrength meter(String s) {
+    if (s == null || s.isBlank()) {
+      return PasswordStrength.INVALID;
     }
+    int flagCnt = 0;
+    if (s.length() >= 8) {
+      flagCnt++;
+    }
+    if (s.matches(".*[0-9].*")) {
+      flagCnt++;
+    }
+    if (s.matches(".*[A-Z].*")) {
+      flagCnt++;
+    }
+
+    if (flagCnt == 1) {
+      return PasswordStrength.WEAK;
+    } else if (flagCnt == 3) {
+      return PasswordStrength.STRONG;
+    } else {
+      return PasswordStrength.NORMAL;
+    }
+  }
 
 }
