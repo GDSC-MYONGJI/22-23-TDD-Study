9816decc49889f485cf560eb1321d8212d38f879
ImKunYoung <ur2kunyoung2@outlook.com>
2023-01-09 15:37:49 +0900

    fix: Fix meets other criteria except for number then normal

--- chap2/src/test/java/com/example/chap2/PasswordStrengthMeter.java
+++ chap2/src/test/java/com/example/chap2/PasswordStrengthMeter.java
@@ -5,6 +5,16 @@ public class PasswordStrengthMeter {
         if (s.length() < 8) {
             return PasswordStrength.NORMAL;
         }
+        boolean containsNum = false;
+        for(char c : s.toCharArray()) {
+            if (c >= '0' && c <= '9') {
+                containsNum = true;
+                break;
+            }
+        }
+        if (!containsNum) {
+            return PasswordStrength.NORMAL;
+        }
         return PasswordStrength.STRONG;
     }
 }
