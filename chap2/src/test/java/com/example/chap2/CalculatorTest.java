21299f4d71762450e6af2b2a0d1b4df1e28b71f9
ImKunYoung <ur2kunyoung2@outlook.com>
2023-01-09 14:59:57 +0900

    test: Add a test case for plus method

---
+++ chap2/src/test/java/com/example/chap2/CalculatorTest.java
@@ -0,0 +1,14 @@
+package com.example.chap2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    @Test
+    void plus() {
+        int result = Calculator.plus(1, 2);
+        assertEquals(3, result);
+    }
+}
