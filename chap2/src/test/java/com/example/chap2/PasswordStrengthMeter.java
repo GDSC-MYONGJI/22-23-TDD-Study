808975d707844b4e2e19c735656c5373ece110af
ImKunYoung <ur2kunyoung2@outlook.com>
2023-01-09 15:33:14 +0900

    fix: fix not meeting all conditions

--- chap2/src/test/java/com/example/chap2/PasswordStrengthMeter.java
+++ chap2/src/test/java/com/example/chap2/PasswordStrengthMeter.java
@@ -2,6 +2,9 @@ package com.example.chap2;
 
 public class PasswordStrengthMeter {
     public PasswordStrength meter(String s) {
+        if (s.length() < 8) {
+            return PasswordStrength.NORMAL;
+        }
         return PasswordStrength.STRONG;
     }
 }
