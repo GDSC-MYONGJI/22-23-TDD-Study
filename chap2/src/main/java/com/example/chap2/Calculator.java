e679394587e7499bd2e98936f069443740c4165c
ImKunYoung <ur2kunyoung2@outlook.com>
2023-01-09 15:19:18 +0900

    feat: Add Calculator Plus

--- chap2/src/main/java/com/example/chap2/Calculator.java
+++ chap2/src/main/java/com/example/chap2/Calculator.java
@@ -3,7 +3,7 @@ package com.example.chap2;
 public class Calculator {
 
     public static int plus(int a1, int a2) {
-        return 3;
+        return a1 + a2;
     }
 
 }
--- chap2/src/test/java/com/example/chap2/CalculatorTest.java
+++ chap2/src/test/java/com/example/chap2/CalculatorTest.java
@@ -8,7 +8,8 @@ public class CalculatorTest {
 
     @Test
     void plus() {
-        int result = Calculator.plus(1, 2);
-        assertEquals(3, result);
+        int sum = Calculator.plus(1, 2);
+        assertEquals(3, sum);
+        assertEquals(5, Calculator.plus(4, 1));
     }
 }
