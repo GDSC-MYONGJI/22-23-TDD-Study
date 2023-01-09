8844b053e8d649a91f61484fd20576c027c8f7d4
ImKunYoung <ur2kunyoung2@outlook.com>
2023-01-09 15:36:07 +0900

    test: Add meets other criteria except for number then normal

--- chap2/src/test/java/com/example/chap2/PasswordStrengthMeterTest.java
+++ chap2/src/test/java/com/example/chap2/PasswordStrengthMeterTest.java
@@ -30,6 +30,13 @@ public class PasswordStrengthMeterTest {
         assertEquals(PasswordStrength.NORMAL, result2);
     }
 
+    /*Meets other criteria except for number then normal*/
+    @Test
+    void meetsOtherCriteria_except_for_number_Then_Normal() {
+        PasswordStrengthMeter meter = new PasswordStrengthMeter();
+        PasswordStrength result = meter.meter("ab!@ABqwer");
+        assertEquals(PasswordStrength.NORMAL, result);
+    }
 
 
 }
