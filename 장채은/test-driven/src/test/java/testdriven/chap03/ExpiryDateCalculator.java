78c1288bfdc8c141e376c47b0df51eddef18d2a0
chaerlo127 <monynony0203@gmail.com>
2023-01-12 15:06:12 +0900

    chap 3: 이만원 이상 납부 예외 처리 #22

--- 장채은/test-driven/src/test/java/testdriven/chap03/ExpiryDateCalculator.java
+++ 장채은/test-driven/src/test/java/testdriven/chap03/ExpiryDateCalculator.java
@@ -4,20 +4,17 @@ package testdriven.chap03;
 import java.time.LocalDate;
 
 public class ExpiryDateCalculator {
-    public LocalDate calculateExpiryDate(LocalDate billingDate, int payAmount){
-        return billingDate.plusMonths(1); // 메서드가 알아서 날짜 주기를 처리해줌
-    }
-
     public LocalDate calculateExpiryDate(PayData payData){
+        int addedMonths = payData.getPayAmount()/10_000;
         if(payData.getFirstBillingDate() != null){
-            LocalDate candidateExp = payData.getBillingDate().plusMonths(1);
+            LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
             // 첫 납부 일자(31일)와 후보 만료일의 일자(28일)이 같지 않을때
             if(payData.getFirstBillingDate().getDayOfMonth() != candidateExp.getDayOfMonth()){
                 // 첫 납부일의 일자를 후보 만료일의 일자로 사용
                 return candidateExp.withDayOfMonth(payData.getFirstBillingDate().getDayOfMonth());
             }
         }
-        return payData.getBillingDate().plusMonths(1);
+        return payData.getBillingDate().plusMonths(addedMonths);
     }
 }
 
