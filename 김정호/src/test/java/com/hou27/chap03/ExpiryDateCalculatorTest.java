a2b5be0d9d04d27ddb291b5dcefb0704be76e162
hou27 <ataj125@gmail.com>
2023-01-12 18:15:53 +0900

    만원을 납부하면 한달 뒤가 만료일 - #19
    가장 쉬운 것부터 테스트

--- 김정호/src/test/java/com/hou27/chap03/ExpiryDateCalculatorTest.java
+++ 김정호/src/test/java/com/hou27/chap03/ExpiryDateCalculatorTest.java
@@ -2,8 +2,17 @@ package com.hou27.chap03;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.time.LocalDate;
 import org.junit.jupiter.api.Test;
 
 public class ExpiryDateCalculatorTest {
+  @Test
+  void 만원_납부하면_한달_뒤가_만료일이_됨() {
+    LocalDate billingDate = LocalDate.of(2019, 3, 1);
+    int payAmount = 10_000;
 
+    ExpiryDateCalculator cal = new ExpiryDateCalculator();
+    LocalDate expiryDate = cal.calculateExpiryDate(billingDate, payAmount);
+    assertEquals(LocalDate.of(2019, 4, 1), expiryDate);
+  }
 }
