f38fdcde944c88a16a488975877f68a38f1584d6
hou27 <ataj125@gmail.com>
2023-01-12 17:21:32 +0900

    테스트 작성 순서 연습 준비 - #19
    납부 금액 기준으로 서비스 만료일 계산 기능 구현을 통해
    테스트 작성 순서를 연습하기 위해 테스트 파일 생성

---
+++ 김정호/src/test/java/com/hou27/chap03/ExpiryDateCalculatorTest.java
@@ -0,0 +1,9 @@
+package com.hou27.chap03;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class ExpiryDateCalculatorTest {
+
+}
--- 김정호/src/test/java/com/hou27/chap03/PasswordStrength.java
+++
@@ -1,5 +0,0 @@
-package com.hou27.chap03;
-
-public enum PasswordStrength {
-  STRONG, WEAK
-}
