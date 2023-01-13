6328b446a2e8d595790fdac7ca88676c50c05949
kyubeom <be_man@naver.com>
2023-01-13 21:44:17 +0900

    폴더 구조 변경 - #25

--- 한규범/src/test/java/chap03/ExpiryDateCalculator.java
+++
@@ -1,9 +0,0 @@
-package chap03;
-
-import java.time.LocalDate;
-
-public class ExpiryDateCalculator {
-    public LocalDate calculateExpiryDate(PayDate payDate){
-        return payDate.getBillingDate().plusMonths(1);
-    }
-}
