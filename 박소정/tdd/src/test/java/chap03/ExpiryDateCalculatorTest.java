12f2dd2b3349adbf5928130946ed0a7e93aa93bc
sojungpp <sophie2458@naver.com>
2023-01-15 21:28:37 +0900

    1달 만료일 계산 - #29

---
+++ 박소정/tdd/src/test/java/chap03/ExpiryDateCalculatorTest.java
@@ -0,0 +1,39 @@
+package chap03;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExpiryDateCalculatorTest {
+
+    @Test
+    void 만원_납부하면_한달_뒤가_만료일이_됨() {
+        assertExpiryDate(
+                LocalDate.of(2019, 3, 1), 10_000,
+                LocalDate.of(2019, 4, 1));
+        assertExpiryDate(
+                LocalDate.of(2019, 5, 5), 10_000,
+                LocalDate.of(2019, 6, 5));
+    }
+
+    @Test
+    void 납부일과_한달_뒤_일자가_같지_않음() {
+        assertExpiryDate(
+                LocalDate.of(2019, 1, 31), 10_000,
+                LocalDate.of(2019, 2, 28));
+        assertExpiryDate(
+                LocalDate.of(2019, 5, 31), 10_000,
+                LocalDate.of(2019, 6, 30));
+        assertExpiryDate(
+                LocalDate.of(2020, 1, 31), 10_000,
+                LocalDate.of(2020, 2, 29));
+    }
+
+    private static void assertExpiryDate(LocalDate billingDate, int payAmount, LocalDate expectedExpiryDate) {
+        ExpiryDateCalculator cal = new ExpiryDateCalculator();
+        LocalDate realExpiryDate = cal.calculateExpiryDate(billingDate, payAmount);
+        assertEquals(expectedExpiryDate, realExpiryDate);
+    }
+}
