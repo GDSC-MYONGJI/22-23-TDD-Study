11d285c272f0e321b97546f5aec6f872ac4fa607
sojungpp <sophie2458@naver.com>
2023-01-15 21:59:58 +0900

    첫 납부일과 만료일 다른 경우 계산 - #29

--- 박소정/tdd/src/test/java/chap03/ExpiryDateCalculator.java
+++ 박소정/tdd/src/test/java/chap03/ExpiryDateCalculator.java
@@ -4,6 +4,13 @@ import java.time.LocalDate;
 
 public class ExpiryDateCalculator {
     public LocalDate calculateExpiryDate(PayData payData) {
+        int addedMonths = 1;
+        if (payData.getFirstBillingDate() != null) {
+            LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
+            if(payData.getFirstBillingDate().getDayOfMonth() != candidateExp.getDayOfMonth()) {
+                return candidateExp.withDayOfMonth(payData.getFirstBillingDate().getDayOfMonth());
+            }
+        }
         return payData.getBillingDate().plusMonths(1);
     }
 }
--- 박소정/tdd/src/test/java/chap03/PayData.java
+++ 박소정/tdd/src/test/java/chap03/PayData.java
@@ -3,15 +3,21 @@ package chap03;
 import java.time.LocalDate;
 
 public class PayData {
+    private LocalDate firstBillingDate;
     private LocalDate billingDate;
     private int payAmount;
 
     private PayData() {
     }
 
-    public PayData(LocalDate billingDate, int payAmount) {
+    public PayData(LocalDate firstBillingDate, LocalDate billingDate, int payAmount) {
+        this.firstBillingDate = firstBillingDate;
         this.billingDate = billingDate;
         this.payAmount = payAmount;
+    }
+
+    public LocalDate getFirstBillingDate() {
+        return firstBillingDate;
     }
 
     public LocalDate getBillingDate() {
@@ -28,6 +34,11 @@ public class PayData {
 
     public static class Builder {
         private PayData data = new PayData();
+
+        public Builder firstBillingDate(LocalDate firstBillingDate) {
+            data.firstBillingDate = firstBillingDate;
+            return this;
+        }
 
         public Builder billingDate(LocalDate billingDate) {
             data.billingDate = billingDate;
