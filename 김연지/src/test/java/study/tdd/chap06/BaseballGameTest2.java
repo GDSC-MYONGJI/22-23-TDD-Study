8d5dffe07b131841515d379c057a1134b50fd9e8
kyj <khkkgon0118@gmail.com>
2023-02-02 22:40:15 +0900

    테스트 코드의 구성 요소 : 상황, 실행, 결과 확인 (방법2) - #62

---
+++ 김연지/src/test/java/study/tdd/chap06/BaseballGameTest2.java
@@ -0,0 +1,25 @@
+package study.tdd.chap06;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class BaseballGameTest2 {
+
+    /*
+    private BaseballGame game;
+
+    @BeforeEach
+    void givenGame() {
+        game = new BaseballGame("456");
+    }
+
+    @Test
+    void exactMatch() {
+        // 실행
+        Score score = game.guess("456");
+        // 결과 확인
+        assertEquals(3, score.strikes());
+        assertEquals(0, score.balls());
+    }
+    */
+}
