e124405a558ba9c7b76359b492d1a9a8fec17dfa
hou27 <ataj125@gmail.com>
2023-02-03 21:47:37 +0900

    외부 상황이 테스트에 영향을 미치지 않도록 - #57

--- 김정호/src/test/java/com/hou27/chap06/ExternalTest.java
+++ 김정호/src/test/java/com/hou27/chap06/ExternalTest.java
@@ -7,8 +7,16 @@ import java.io.FileNotFoundException;
 import org.junit.jupiter.api.Test;
 
 public class ExternalTest {
+  private void givenNoFile(String path) {
+    File file = new File(path);
+    if(file.exists()) {
+      file.delete();
+    }
+  }
   @Test
   void noDataFile_Then_Exception() {
+    givenNoFile("badpath.txt");
+
     File dataFile = new File("badpath.txt");
     assertThrows(IllegalArgumentException.class, () -> {
       new DataScanner(dataFile);
