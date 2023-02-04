349a20ebfdef1621084552af57482536c0a08240
JungChangwoo <ikkwik02@naver.com>
2023-02-04 19:10:44 +0900

    [#60] add LifeCycleTest

---
+++ 정창우/src/test/java/com/example/tdd_project/chapter5/LifecycleTest.java
@@ -0,0 +1,35 @@
+package com.example.tdd_project.chapter5;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class LifecycleTest {
+
+    public LifecycleTest(){
+        System.out.println("new Lifecycle Test");
+    }
+
+    @BeforeEach
+    void setUp(){
+        System.out.println("setUp");
+    }
+
+    @Test
+    void a() {
+        System.out.println("A");
+    }
+
+    @Test
+    void b() {
+        System.out.println("B");
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("tearDown");
+    }
+
+}
