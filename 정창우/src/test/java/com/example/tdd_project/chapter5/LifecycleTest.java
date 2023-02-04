9c1b624888d2edf7b497dc59592dac7e86cd536d
JungChangwoo <ikkwik02@naver.com>
2023-02-04 19:41:44 +0900

    [#60] add displayName

--- 정창우/src/test/java/com/example/tdd_project/chapter5/LifecycleTest.java
+++ 정창우/src/test/java/com/example/tdd_project/chapter5/LifecycleTest.java
@@ -1,10 +1,9 @@
 package com.example.tdd_project.chapter5;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@DisplayName("Lifecycle 테스트")
 @SpringBootTest
 public class LifecycleTest {
 
@@ -12,21 +11,26 @@ public class LifecycleTest {
         System.out.println("new Lifecycle Test");
     }
 
+    @DisplayName("준비 작업")
     @BeforeEach
     void setUp(){
         System.out.println("setUp");
     }
 
+    @DisplayName("A 프린트 테스트")
     @Test
     void a() {
         System.out.println("A");
     }
 
+    @DisplayName("미완성 테스트")
+    @Disabled
     @Test
     void b() {
         System.out.println("B");
     }
 
+    @DisplayName("마무리 작업")
     @AfterEach
     void tearDown() {
         System.out.println("tearDown");
