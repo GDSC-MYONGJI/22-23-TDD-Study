a7e8461b87adb329eda4aa28d23a79ac4f852b3a
chaerlo127 <monynony0203@gmail.com>
2023-02-04 21:58:58 +0900

    chap 6: BeforeEach를 적용한 메서드에서 상황을 설정 #61

--- 장채은/test-driven/src/test/java/testdriven/chap06/chap06Test.java
+++ 장채은/test-driven/src/test/java/testdriven/chap06/chap06Test.java
@@ -1,14 +1,19 @@
 package testdriven.chap06;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class chap06Test {
+    BaseballGame game;
+    @BeforeEach // 상황 설정
+    void giveenGame(){
+        game = new BaseballGame("456");
+    }
+
     @Test
     void exactMatch(){
-        BaseballGame game = new BaseballGame("456");
-
         Score score = game.guess("456");
         assertEquals(3, score.strikes());
         assertEquals(0, score.balls());
@@ -16,7 +21,6 @@ public class chap06Test {
 
     @Test
     void noMatch(){
-        BaseballGame game = new BaseballGame("123");
         Score score = game.guess("456");
 
         assertEquals(0, score.strikes());
