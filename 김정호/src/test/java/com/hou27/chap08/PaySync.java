5ea3fb6259926668e82955828a192474161d65ed
hou27 <ataj125@gmail.com>
2023-02-11 18:41:57 +0900

    하드 코딩된 경로를 사용한 경우 - #79
    파일 경로가 하드 코딩되어있어
    테스트하려면 반드시 해당 경로에 파일이 위치해야 한다.
    이런 경로 뿐만 아니라 하드 코딩된 ip, port 정보도
    테스트를 어렵게 만든다.

---
+++ 김정호/src/test/java/com/hou27/chap08/PaySync.java
@@ -0,0 +1,21 @@
+package com.hou27.chap08;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Collectors;
+
+public class PaySync {
+  private PayInfoDao payInfoDao = new PayInfoDao();
+
+  public void sync() {
+    // 하드 코딩된 경로를 사용하고 있다.
+    Path path = Path.of("tmp/paydata/payinfo.csv");
+    List<PayInfo> payInfos = Files.lines(path)
+        .map(line -> {
+          String[] data = line.split(",");
+          return new PayInfo(data[0], Integer.parseInt(data[1]));
+        }).collect(Collectors.toList());
+
+    payInfos.forEach(pi -> payInfoDao.insert(pi));
+  }
+}
