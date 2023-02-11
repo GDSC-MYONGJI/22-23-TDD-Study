41a315488d8d32d85af7012c23abb750b99d6d70
hou27 <ataj125@gmail.com>
2023-02-11 23:31:43 +0900

    테스트하고 싶은 코드 분리 - #79
    테스트하고 싶은 코드를 따로 분리하여
    해당 기능만 테스트할 수 있다.

--- 김정호/src/test/java/com/hou27/chap08/UserPointCalculator.java
+++ 김정호/src/test/java/com/hou27/chap08/UserPointCalculator.java
@@ -21,8 +21,27 @@ public class UserPointCalculator {
 
     Product p = productDao.selectById(s.getProductId());
     LocalDate now = LocalDate.now(); // 현재 날짜를 구한다.
+    PointRule rule = new PointRule();
+    int point = rule.calculate(s, p, now);
+//    int point = 0;
+//    if(s.isFinished(now)) { // 현재 시간에 따라 결과가 달라진다.
+//      point += p.getDefaultPoint();
+//    } else {
+//      point += p.getDefaultPoint() + 10;
+//    }
+//
+//    if(s.getGrade() == GOLD) {
+//      point += 100;
+//    }
+
+    return point;
+  }
+}
+
+class PointRule {
+  public int calculate(Subscription s, Product p, LocalDate now) {
     int point = 0;
-    if(s.isFinished(now)) { // 현재 시간에 따라 결과가 달라진다.
+    if(s.isFinished(now)) {
       point += p.getDefaultPoint();
     } else {
       point += p.getDefaultPoint() + 10;
