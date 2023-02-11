4f49c7854d5cfe94288f300a65841b6bfa863546
hou27 <ataj125@gmail.com>
2023-02-11 22:22:57 +0900

    하드 코딩된 상수를 메서드 파라미터로 받기 - #79
    파일 경로를 변경할 수 있는 메서드를 구현하여
    경로를 교체할 수 있도록 함.
    경우에 따라 생성자를 통해 받게 작성할 수도 있다.

--- 김정호/src/test/java/com/hou27/chap08/PaySync.java
+++ 김정호/src/test/java/com/hou27/chap08/PaySync.java
@@ -1,5 +1,6 @@
 package com.hou27.chap08;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.stream.Collectors;
@@ -7,10 +8,15 @@ import java.util.stream.Collectors;
 public class PaySync {
   // 의존 객체를 직접 생성하고 있다.
   private PayInfoDao payInfoDao = new PayInfoDao();
+  private String filePath = "tmp/paydata/payinfo.csv";
 
-  public void sync() {
-    // 하드 코딩된 경로를 사용하고 있다.
-    Path path = Path.of("tmp/paydata/payinfo.csv");
+  // 경로를 설정 가능하게 메서드 생성
+  public void setFilePath(String filePath) {
+    this.filePath = filePath;
+  }
+
+  public void sync() throws IOException {
+    Path path = Path.of(filePath);
     List<PayInfo> payInfos = Files.lines(path)
         .map(line -> {
           String[] data = line.split(",");
